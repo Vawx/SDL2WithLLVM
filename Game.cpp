83aae81cbb2544f8ec934854e699da1d02a847d8
kyle <altck8@gmail.com>
2016-03-10 16:52:15 -0800

    Added timing code to preven process heavy rendering

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,11 @@
 
 std::vector<LTexture*> TextureList;
 
+float DeltaTime;
+
 Game LGame;
+Player LPlayer;
+Timing LTiming;
 
 void InitGame( char* ScreenName, int Width, int Height )
 {
@@ -20,6 +24,7 @@ void InitGame( char* ScreenName, int Width, int Height )
                     LGame.ScreenWidth = Width;
                     LGame.ScreenHeight = Height;
                     InitBackground( );
+                    InitPlayer( );
                     LGame.bRunning = true;
                     MainLoop( );   
                 }
@@ -47,22 +52,69 @@ void InitGame( char* ScreenName, int Width, int Height )
 void MainLoop( )
 {
     while( LGame.bRunning )
-    {
-        SDL_RenderClear( LGame.Renderer );
+    { 
+        TimingBegin( );
         
+        // Render
+        SDL_RenderClear( LGame.Renderer );
         for( size_t i = 0; i < LGame.Sprites.size( ); i++ )
         {
-           SDL_Rect  renderQuad = 
+            SDL_Rect renderSrcQuad = { 0,0,0,0 };
+            
+            // Game
+            if( LGame.Sprites[ i ]->Texture->Type == TT_GROUND )
+            {
+                LGame.Sprites[ i ]->SpriteLoc.X -= 1.f;
+                if( LGame.Sprites[ i ]->SpriteLoc.X + LGame.ScreenWidth <= 0 )
+                {
+                    LGame.Sprites[ i ]->SpriteLoc.X = LGame.ScreenWidth;
+                }
+            }
+            else if( LGame.Sprites[ i ]->Texture->Type == TT_PLAYER )
+            {
+                static int playerFrameCount;
+                playerFrameCount++;
+              
+                if( playerFrameCount > 10 )
+                {
+                    playerFrameCount = 0;
+                    LPlayer.X += 32;
+                    if( LPlayer.X >= LPlayer.MaxX )
+                    {
+                        LPlayer.X = 0;
+                        LPlayer.Y += 32;
+                        if( LPlayer.Y >= LPlayer.MaxY )
+                        {
+                            LPlayer.Y = 0;
+                        }
+                    }
+                }
+                renderSrcQuad.x = LPlayer.X;
+                renderSrcQuad.y = LPlayer.Y;
+                renderSrcQuad.w = 32;
+                renderSrcQuad.h = 32;
+            }
+
+
+            SDL_Rect  renderQuad = 
             {   LGame.Sprites[ i ] ->SpriteLoc.X,
                 LGame.Sprites[ i ] ->SpriteLoc.Y,
                 LGame.Sprites[ i ] ->Width, 
                 LGame.Sprites[ i ] ->Height
             };
-            
-            SDL_RenderCopy( LGame.Renderer, LGame.Sprites[ i ]->Texture->Image, NULL, &renderQuad );
+
+            if(  LGame.Sprites[ i ]->Texture->Type != TT_PLAYER  )
+            {
+                SDL_RenderCopy( LGame.Renderer, LGame.Sprites[ i ]->Texture->Image,  NULL, &renderQuad );
+            }
+            else
+            {
+                SDL_RenderCopyEx( LGame.Renderer, LGame.Sprites[ i ]->Texture->Image, &renderSrcQuad, &renderQuad, 0, NULL, SDL_FLIP_HORIZONTAL );
+            }
        }
         SDL_RenderPresent( LGame.Renderer );
 
+        // Input
         SDL_Event e;
         while( SDL_PollEvent( &e ) )
         {
@@ -79,8 +131,9 @@ void MainLoop( )
                     break;
                 }
             }
-
         }
+        
+        printf( "FPS: %f\n", TimingEnd( ) );
     }
 
     for( size_t i = 0; i < LGame.Sprites.size( ); i++ )
@@ -95,7 +148,51 @@ void MainLoop( )
     SDL_Quit( );
 }
 
-Sprite* CreateSprite( int X, int Y, int Width, int Height, char* Filepath )
+void TimingBegin( )
+{
+    LTiming.m_StartTicks = SDL_GetTicks( );
+}
+
+void TimingCalculateFPS( )
+{
+    static const int NUM_SAMPLES = 10;
+	static float frameTimes[ NUM_SAMPLES ];
+	static int currentFrame = 0;
+	static float previousTicks = SDL_GetTicks( );
+	float currentTicks = SDL_GetTicks( );
+
+	LTiming.m_FrameTime = currentTicks - previousTicks;
+	frameTimes[ currentFrame % NUM_SAMPLES ] = LTiming.m_FrameTime;
+	previousTicks = currentTicks;
+
+	int count; 
+	currentFrame++;
+	count = ( currentFrame < NUM_SAMPLES ) ? currentFrame : NUM_SAMPLES;
+
+	float frameTimeAverage = 0;
+	for ( int i = 0; i < count; i++ )
+	{
+		frameTimeAverage += frameTimes[ i ];
+	}
+
+	frameTimeAverage /= count;
+	LTiming.m_FPS = ( frameTimeAverage > 0 ) ? 1000.f / frameTimeAverage : 60.f;
+}
+
+float TimingEnd( )
+{
+    TimingCalculateFPS( );
+    float frameTicks = SDL_GetTicks( ) - LTiming.m_StartTicks;
+	if( 1000.f / LTiming.m_MaxFPS >= frameTicks )
+	{
+		SDL_Delay( int( 1000.f / LTiming.m_MaxFPS - frameTicks ) );
+	}
+	LTiming.m_End = SDL_GetTicks( );
+	DeltaTime = ( LTiming.m_End - LTiming.m_StartTicks ) * 0.001f;
+	return LTiming.m_FPS;
+}
+
+Sprite* CreateSprite( int X, int Y, int Width, int Height, char* Filepath, TextureType Type )
 {
     SDL_Texture *asImage  = NULL;
     SDL_Surface *fromFile = IMG_Load( Filepath );
@@ -112,6 +209,7 @@ Sprite* CreateSprite( int X, int Y, int Width, int Height, char* Filepath )
             
             resultTexture->Image = asImage;
             resultTexture->Filepath = Filepath;
+            resultTexture->Type = Type;
             
             Vector2 resultLoc;
             resultLoc.X = X;
@@ -139,16 +237,26 @@ void InitBackground( )
 {
     char* background = "./content/img/background/a1.png";
     char* trees = "./content/img/background/a2.png";
+    char* bigtree = "./content/img/background/a3.png";
     
-    LGame.Sprites.push_back( CreateSprite( 0, 0, LGame.ScreenWidth, LGame.ScreenHeight, background ) );
-            
+    LGame.Sprites.push_back( CreateSprite( 0, 0, LGame.ScreenWidth, LGame.ScreenHeight, background, TT_BACKGROUND ) );
+
     Vector2 startingLocation;
     startingLocation.X = 0;
-    startingLocation.Y = 545;
+    startingLocation.Y = 482;
     for( int i = 0; i < 7; i++ )
     {
-         LGame.Sprites.push_back( CreateSprite( startingLocation.X, startingLocation.Y, 160, 224, trees  ) );
+        LGame.Sprites.push_back( CreateSprite( startingLocation.X, startingLocation.Y, 160, 224, trees, TT_BACKGROUND  ) );
         startingLocation.X += 160;
     }
+
+    LGame.Sprites.push_back( CreateSprite( 0, 396, LGame.ScreenWidth, LGame.ScreenHeight / 2, bigtree, TT_GROUND ) );
+    LGame.Sprites.push_back( CreateSprite( LGame.ScreenWidth, 396, LGame.ScreenWidth, LGame.ScreenHeight / 2, bigtree, TT_GROUND ) );
+}
+
+void InitPlayer( )
+{
+    char* sheet = "./content/img/player/a1.png";    
     
+    LGame.Sprites.push_back( CreateSprite( 256, 672, 64, 64, sheet, TT_PLAYER ) );
 }
--- Game.h
+++ Game.h
@@ -5,16 +5,24 @@
 #include <SDL2/SDL_image.h>
 #include <vector>
 
+typedef enum __TEXTURETYPE__
+{
+    TT_BACKGROUND,
+    TT_GROUND,
+    TT_PLAYER,
+} TextureType;
+
 struct LTexture
 {
+    TextureType Type;
     char* Filepath;  
     SDL_Texture* Image;
 };
 
 struct Vector2
 {
-    int X;  
-    int Y;
+    float X;  
+    float Y;
 };
 
 struct Sprite
@@ -24,6 +32,23 @@ struct Sprite
     int Height;
     LTexture* Texture;
     char* Filepath;
+};
+
+struct Ground
+{
+    int X;  
+    int Y;
+};
+
+struct Player
+{
+    const int MaxX = 128;
+    const int MaxY = 128;
+    int CurrentX = 0;
+    int CurrentY = 0;
+    int X;
+    int Y;
+    Vector2 Position;
 };
 
 struct Game
@@ -37,9 +62,25 @@ struct Game
     std::vector<Sprite*> Sprites;
 };
 
+struct Timing 
+{
+    const float m_MaxFPS = 60;
+    const float m_TargetFPS = 60;
+	float m_FPS;
+	float m_FrameTime;
+	float DeltaTime;
+    float m_End;
+	unsigned int m_StartTicks;  
+};
+
 void InitGame( char* ScreenName, int Width, int Height );
 void MainLoop( );
 
-void InitBackground( );
+void TimingBegin( );
+void TimingCalculateFPS( );
+float TimingEnd( );
 
-Sprite* CreateSprite( int X, int Y, int Width, int Height, char* Filepath );
+void InitBackground( );
+void InitPlayer( );
+
+Sprite* CreateSprite( int X, int Y, int Width, int Height, char* Filepath, TextureType );
